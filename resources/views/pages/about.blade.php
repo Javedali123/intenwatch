12d6441917c710f3768f6368bc4e7e61bfd965ac
Javed Ali <u1669848@unimail.hud.ac.uk>
2017-01-19 15:50:11 +0000

    organised files and folders using partials
     - Added footer, head, nav, javascript partials
    
     - created a partials folder 
    
     - cleaned up the code further

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('main')
 
+@section('title', '| About Us')
+
 @section('content')
     <div class="row">
       <div class="col-md-12">
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+
+<p class="text-center">Copyright Javed - All rights Reserved</p>
