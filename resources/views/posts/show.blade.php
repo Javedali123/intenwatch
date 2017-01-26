a7eaf087c63ad3db6fec4897c5e1ac898a36c73b
Javed Ali <u1669848@unimail.hud.ac.uk>
2017-01-26 23:27:50 +0000

    welcome page now shows the most recent blog posts
       -  edited welcome page to pull the recent posts
      - created a back button for view posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -38,9 +38,17 @@
 
 
        </div>
+      </div>
+
+       <div class="row">
+         <div class="col-md-12">
+           {{ Html::linkroute('posts.index', '<<See All Posts', [], ['class' => 'btn btn-default btn-block btn-h1-spacing']) }}
+         </div>
+       </div>
 
       </div>
     </div>
+  </div>
 
 
 @endsection
