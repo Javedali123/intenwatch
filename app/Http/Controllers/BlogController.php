69c7516b7731ac1836137c3a151dc3588d1fc511
Javed Ali <u1669848@unimail.hud.ac.uk>
2017-01-29 16:19:25 +0000

    Created an archive page for blog posts
        -  added index.blade.php  to show all posts
        - added navigation button for blog 
       - fixed navigation bugs 
       - cleaned up code

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Post;
+use App\Post;// uses post model
 
 class BlogController extends Controller
 {
     //
+
+    public function getIndex() {
+      $post = Post::paginate(2); // display 10 posts
+
+        return view('blog.index')->withPosts($post);
+    }
     public function getSingle($slug) {
       // fetch from database based on slug
       $post = Post::where('slug', '=', $slug)->first(); // get first and stop
