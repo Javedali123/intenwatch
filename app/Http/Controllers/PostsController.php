53469f19708306f36dcf6e6763ec184deb9dee05
Javed Ali <u1669848@unimail.hud.ac.uk>
2017-01-29 15:21:36 +0000

    Fixed a bug in the slug feature
        -  editing posts can now be achieved without an error 
    
      - added an if statement to posts controller update section
    
       - validation now working correctly

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -101,11 +101,20 @@ class PostsController extends Controller
     public function update(Request $request, $id)
     {
         //validate the data
+        $post = Post::find($id);
+        if ($request->input('slug') == $post->slug) {
+          $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+          ));  
+
+        } else {
         $this->validate($request, array(
           'title' => 'required|max:255',
           'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
           'body' => 'required'
         ));
+      }
 
         // save data to the database
         $post = Post::find($id);
