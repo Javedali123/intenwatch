94cf3ff14650d56fb27992243820720f8965a7e7
Javed Ali <u1669848@unimail.hud.ac.uk>
2018-03-15 11:42:43 +0000

    Minor Changes to Footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,3 @@
 <hr>
 
-<p class="text-center">----------------------Javed Ali U1669848, My Laravel Blog Project @2017------------------------</p>
+<p class="text-center">----------------------, My Laravel Blog Project @2018------------------------</p>
