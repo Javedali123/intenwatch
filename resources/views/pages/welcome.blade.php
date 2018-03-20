b5fedf7a95c7cbb8df9e8f8c4610471003ba1920
Javed Ali <u1669848@unimail.hud.ac.uk>
2018-03-20 11:54:38 +0000

    Added Correct logo, changed background of jumbotron
    All Changes on Welcome Page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,15 +5,29 @@
 @section('content')
   <div class="row">
       <div class="col-md-12">
-        <div class="jumbotron jumbotron-fluid">
+        <div class="jumbotron jumbotron-fluid" style="background-color:#32CD32">
+
+
           <style>
           img {
               float: left;
           }
           </style>
-          <img src="canonlogo.png" alt="Mountain View" style="width:300px;height:200px;">
+
+        <!--  <video width="320" height="240" autoplay loop>
+  <source src="IntenwatchSpinLogo.mp4" type="video/mp4" />
+
+  Your browser does not support the video tag.
+</video> -->
+       <img src="intenwatch.png" alt="Intenwatch Logo " style="width:350px;height:200px;">
           <h1>Intenwatch.com</h1>
           <p class="lead">International Energy Watch</p>
+
+        <br />
+
+          <p>
+
+          </p> </p>
 
         </div>
       </div>
@@ -39,7 +53,7 @@
      <div class="col-md-3 col-md-offset-1">
 
         <div class="jumbotron">
-          <h3>Latest Posts</h3>
+          <h3>Latest News</h3>
           <h3></h3>
 
         </div>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,3 @@
 <hr>
 
-<p class="text-center">-----------Intenwatch Ltd. Registered U.K. Office. 6a Pickering Street, Leeds, LS12 2QG  CopyRight Intenwatch LTD © 2018 all rights reserved----------------</p>
+<p class="text-center">-----------Intenwatch Ltd. Registered U.K. Office. 6a Pickering Street, Leeds, LS12 2QG | CopyRight Intenwatch LTD © 2018 all rights reserved----------------</p>
