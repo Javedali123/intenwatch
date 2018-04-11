53d5ef07901290fa150a08f0e1cdd36b1dc1d174
Javed Ali <u1669848@unimail.hud.ac.uk>
2018-04-11 12:00:16 +0100

    Added all presentation videos
     - Linked youtube videos 
     - Styled Iframes
     - added description

--- resources/views/pages/presentations.blade.php
+++ resources/views/pages/presentations.blade.php
@@ -9,9 +9,120 @@
         <div class="jumbotron jumbotron-fluid" style="background-color:#e2edff">
           <h2 class="display-4">Intenwatch Presentations</h2>
            <hr class="my-4">
+           <style>
+             iframe {
+                 float: left;
+             }
+           </style>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/1sTKb9Vjkek"
 
-        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen; style="margin: 0 2em 0 0em; padding:1px;border:1px solid black;" ></iframe>
+
+<h2> What is a Raspberry Pi? </h2>
+        <p class="lead">
+          The Raspberry Pi is a credit-card-sized computer that plugs into your TV and a keyboard.
+          It is a capable little computer which can be used in electronics projects, and for many of the things that
+          your desktop PC does,
+          like spreadsheets, word processing, browsing the internet, and playing games.
+
+        </p>
+<br />
+<hr />
+<br />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/kVXtsvIUQ_Y"
+
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+
+        style="margin: 0 2em 0 0em; padding:1px;border:1px solid black;"></iframe>
+
+        <h2> Intenwatch Version 6 </h2>
+                <p class="lead">
+
+
+                </p>
+
+                <br />
+                <hr />
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+
+                <br />
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Y5GfeC1QSgU"
+
+frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+
+style="margin: 0 2em 0 0em; padding:1px;border:1px solid black;"></iframe>
+
+<h2> Intenwatch Harmonics Presentation Video </h2>
+        <p class="lead">
+
+
+        </p>
+
+        <br />
+        <hr />
+        <br />
+        <br />
+        <hr />
+        <br />
+        <br />
+        <hr />
+        <br />
+        <br />
+
+
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/fByLurHh8ow"
+
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+
+        style="margin: 0 2em 0 0em; padding:1px;border:1px solid black;"></iframe>
+
+        <h2> Intenwatch Harmonics Presentation Video </h2>
+                <p class="lead">
+
+
+                </p>
+
+                <br />
+                <hr />
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/SXUdvzzWTJI"
+
+                frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+
+                style="margin: 0 2em 0 0em; padding:1px;border:1px solid black;"></iframe>
+                <h2> Intenwatch Harmonics Presentation Video </h2>
+                        <p class="lead">
+
+
+                        </p>
+
+                        <br />
+                        <hr />
+                        <br />
+                        <br />
+                        <hr />
+                        <br />
+                        <br />
+                        <hr />
+                        <br />
+                        <br />
 
           </div>
       </div>
