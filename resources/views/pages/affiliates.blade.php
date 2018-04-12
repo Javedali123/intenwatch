f34c814d11d56d289fea3c091fec5f896a91b550
Javed Ali <u1669848@unimail.hud.ac.uk>
2018-04-12 11:22:23 +0100

    Minor changes
     - cleaned up code

--- resources/views/pages/affiliates.blade.php
+++ resources/views/pages/affiliates.blade.php
@@ -13,131 +13,116 @@
           <hr />
           <a href="http://microstarelectric.com/">
            <img src="microstar.png" alt="microstar Logo " style="width:200px;height:50px;">
-         </a>
-           <br />
-           <br />
-           <br />
-
-           <p>
-             Microstar Electric Company Ltd. Nanjing. China.
-           </p>
-
-           <hr />
-           <a href="http://www.boxergifts.com/">
-            <img src="boxer.png" alt="microstar Logo " style="width:200px;height:50px;">
           </a>
+          <br />
+          <br />
+          <br />
+          <p>
+             Microstar Electric Company Ltd. Nanjing. China.
+          </p>
 
-            <br />
-            <br />
-            <br />
+          <hr />
+          <a href="http://www.boxergifts.com/">
+            <img src="boxer.png" alt="boxergifts Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Boxer Games. Leeds. England.
+          </p>
 
-            <p>
-              Boxer Games. Leeds. England.
-            </p>
+          <hr />
+          <a href="http://www.lonestargroup.com/en/lonestar-leeds.html">
+            <img src="lonestar.png" alt="lonestar Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Lonestar Precision Engineering LWD Ltd. Leeds. England.
+          </p>
 
-            <hr />
-            <a href="http://www.lonestargroup.com/en/lonestar-leeds.html">
-              <img src="lonestar.png" alt="microstar Logo " style="width:200px;height:50px;">
-</a>
-              <br />
-              <br />
-              <br />
+          <hr />
+          <a href="https://sydney.edu.au/">
+            <img src="sydney.svg" alt="sydney university Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            University of Sydney. Australia.
+          </p>
 
-              <p>
-                Lonestar Precision Engineering LWD Ltd. Leeds. England.
-              </p>
+          <hr />
+          <a href="http://www.solutions-lighting.co.uk/">
+            <img src="solutions.jpg" alt="solutions lighting Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Solutions Lighting Limited. Leeds. England.
+          </p>
 
-              <hr />
-<a href="https://sydney.edu.au/">
-              <img src="sydney.svg" alt="microstar Logo " style="width:200px;height:50px;">
-</a>
-              <br />
-              <br />
-              <br />
-
-              <p>
-                University of Sydney. Australia.
-              </p>
-
-              <hr />
-<a href="http://www.solutions-lighting.co.uk/">
-              <img src="solutions.jpg" alt="microstar Logo " style="width:200px;height:50px;">
-</a>
-              <br />
-              <br />
-              <br />
-
-              <p>
-                Solutions Lighting Limited. Leeds. England.
-              </p>
-
-              <hr />
-<a href="http://www.cannon-electrical.co.uk/">
-              <img src="cannonlogo2.png" alt="microstar Logo " style="width:200px;height:50px;">
-</a>
-              <br />
-              <br />
-              <br />
-
-              <p>
-                Cannon Electrical Services Ltd, Leeds. England.
-              </p>
-
-
-                            <hr />
-
-                            <!--<img src="cannonlogo2.png" alt="microstar Logo " style="width:200px;height:50px;">-->
-
-                            <br />
-                            <br />
-                            <br />
-
-                            <p>
-                              Bradford Cathedral. England.
-                            </p>
-
-                            <hr />
-<a href="http://www.pennington.co.uk/">
-                            <img src="jpl.png" alt="microstar Logo " style="width:200px;height:50px;">
-</a>
-                            <br />
-                            <br />
-                            <br />
-
-                            <p>
-                              Jack Pennington Ltd. Bradford. England.
-                            </p>
-
-                            <hr />
-
-                            <img src="pi.png" alt="microstar Logo " style="width:200px;height:50px;">
-
-                            <br />
-                            <br />
-                            <br />
-
-                            <p>
-                              PiNestHub. Bradford. England.
-                            </p>
-
-                            <hr />
-
-                            <!--<img src="jpl.png" alt="microstar Logo " style="width:200px;height:50px;">-->
-
-                            <br />
-                            <br />
-                            <br />
-
-                            <p>
-                              Airedale Computer Recycling. Castleford, England.
-                            </p>
+          <hr />
+          <a href="http://www.cannon-electrical.co.uk/">
+            <img src="cannonlogo2.png" alt="cannon electrical Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Cannon Electrical Services Ltd, Leeds. England.
+          </p>
 
 
+          <hr />
+          <a href="http://www.bradfordcathedral.org/">
+            <img src="cathedral.png" alt="bradford cathedral Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Bradford Cathedral. England.
+          </p>
 
+          <hr />
+          <a href="http://www.pennington.co.uk/">
+            <img src="jpl.png" alt="jpl pennington Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Jack Pennington Ltd. Bradford. England.
+          </p>
+
+          <hr />
+          <a href="http://www.pinest.co.uk/">
+            <img src="pinest.png" alt="pinest Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            PiNestHub. Bradford. England.
+          </p>
+
+          <hr />
+          <a href="https://www.airedalecomputerrecycling.co.uk/">
+           <img src="airedale.png" alt="airedale recycling Logo " style="width:200px;height:50px;">
+          </a>
+          <br />
+          <br />
+          <br />
+          <p>
+            Airedale Computer Recycling. Castleford, England.
+          </p>
 
         </div>
       </div>
-      </div> <!-- end of header .row -->
+    </div> <!-- end of header .row -->
 
-
-      @endsection
+@endsection
